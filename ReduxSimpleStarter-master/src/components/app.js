c9c254b5cfeea20940cc0aac42218e7a6f1a7dea
mac <ksngho7@gmail.com>
2018-04-10 15:34:38 -0400

    react changes

--- ReduxSimpleStarter-master/src/components/app.js
+++ ReduxSimpleStarter-master/src/components/app.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import BookList from '../containers/book-list';
+import NavBar from './nav_bar';
+import About from './about';
+
 
 export default class App extends Component {
   render() {
     return (
         <div>
-          <BookList/>
+            <NavBar/>
+            <About/>
         </div>
     );
   }
