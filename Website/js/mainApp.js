6da27632302827c16fe596d18109e004f4b69f42
Boneless Savage <bonelesscoding@gmail.com>
2018-04-12 01:24:28 -0400

    added back lines so it won't send twice in form step 2

--- Website/js/mainApp.js
+++ Website/js/mainApp.js
@@ -17,8 +17,8 @@ const addressInput = document.getElementById('nameInput').value;
 form.addEventListener('submit', e => {
     e.preventDefault();
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-//        .then(response => console.log('Success!', response))
-//        .catch(error => console.error('Error!', error.message));
+        .then(response => console.log('Success!', response))
+        .catch(error => console.error('Error!', error.message));
      document.getElementById('submitBtn').addEventListener('click', stepThree);
 
 });
